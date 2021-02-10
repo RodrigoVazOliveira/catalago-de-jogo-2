13ec54abfae0aa9c09d970ca9bc3345b7a9793cb
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-10 17:14:42 -0300

    implementado modelo de jogo de tabuleiro

---
+++ src/br/dev/rvz/JogoDeTabuleiro.java
@@ -0,0 +1,26 @@
+package br.dev.rvz;
+
+/**
+ * modelo de jogo de tabuleiro
+ * @author Rodrigo.vaz
+ * */
+public class JogoDeTabuleiro extends Jogo {
+
+    private int quantidadeDePeca;
+
+    public JogoDeTabuleiro() {
+    }
+
+    public JogoDeTabuleiro(String nome, String quantidadeDeJogador, int horaDeJogo, TipoDeJogo tipoDeJogo, int quantidadeDePeca) {
+        super(nome, quantidadeDeJogador, horaDeJogo, tipoDeJogo);
+        this.quantidadeDePeca = quantidadeDePeca;
+    }
+
+    public int getQuantidadeDePeca() {
+        return quantidadeDePeca;
+    }
+
+    public void setQuantidadeDePeca(int quantidadeDePeca) {
+        this.quantidadeDePeca = quantidadeDePeca;
+    }
+}
